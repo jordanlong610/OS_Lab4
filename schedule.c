da68006b9ea2571617d27884fd51c6f7f27057bc
Jordan Long <jordan@promogroupllc.com>
2018-04-03 01:36:15 -0400

    Program working

--- schedule.c
+++ schedule.c
@@ -19,73 +19,71 @@
 sem_t timer;
 sem_t ready;
 int timeQ;
+int threadsCreated;
+int threadsFinished = 0;
 int* burstTime;
+int globalTime = 0;
 
-int updateThreadTime(int, int, int);
-
-void *threadTime(void *input)
+/*
+ * Runner for each spawned thread. Will wait for its turn, then run for allotted burst time until finished.
+ */
+void *threadRunner(void *number)
 {
-  int time = 0;
-  int threadNumber = atoi(input);
-  while(time < burstTime[threadNumber])
+  int threadTime = 0;
+  int threadNumber = *((int *) number);
+  while(threadTime < burstTime[threadNumber])
   {
-    sem_wait(&timer);
-    time = updateThreadTime(time, burstTime[threadNumber], threadNumber);
+	  sem_wait(&timer);
+	  printf("Thread %d now being executed.\n", threadNumber);
+	  	int i = 0;
+	  	while(i < timeQ && threadTime < burstTime[threadNumber]+1)
+	  	{
+	  	  sleep(1);
+	  	  threadTime++;
+	  	  i++;
+	  	}
   }
-  printf("Thread %d is finished!\n", threadNumber);
+  printf("Thread %d finished in %d seconds! It will now be terminated.\n", threadNumber, globalTime);
+  threadsFinished++;
   pthread_exit(0);
-}
-
-int updateThreadTime(int timer, int burstTime, int threadNumber)
-{
-  printf("Thread %d now being executed.\n", threadNumber);
-  int i = 0;
-  while(i < timeQ && timer < burstTime)
-  {
-    sleep(1);
-    timer++;
-    fflush(stdout);
-    i++;
-  }
-  return timer;
 }
 
 /*
  * Timer will call sem post every time the time reaches the timeQuantum
  * When all threads finish, timer thread will exit.
  */
-void *timerTime()
+void *timerFunction()
 {
-	int time = 0;
+	globalTime = 0;
 	int interval = timeQ;
-	while(1)
+	while(threadsFinished != threadsCreated)
 	{
-		printf("%d\n", time);
-		sleep(1);
-		time++;
 
-		if(time == interval)
+		if(globalTime == interval)
 		{
 			interval += timeQ;
 			sem_post(&timer);
 		}
+		else
+		{
+			printf("%d\n", globalTime);
+			sleep(1);
+			globalTime++;
+		}
     }
-	printf("All threads finished.");
+	printf("All threads finished. Scheduler thread will now exit.");
 	pthread_exit(0);
 }
 
 
-
 /*
  * Main method for the thread scheduling program.
+ * Will check inputs, request burst times for each thread, and then spawn the threads.
  */
 int main(int argc, char **argv)
 {
-
-
 	sem_init(&ready, 0, 0);
 	sem_init(&timer, 0, 1);
-
 
 	/*
 	 * If integer is not included when the program is run, throws error
@@ -106,21 +104,19 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
-
-	int threadsCreated = atoi(argv[1]);
+	threadsCreated = atoi(argv[1]);
 	timeQ = atoi(argv[2]);
 	int BT[threadsCreated]; //Point local burst times at global array
 	burstTime = BT;
 
 	/*
-	 * Prints out requests for burst times of each thread, and stores value in array burstTime.
+	 * Prints out requests for burst times of each thread, and stores value in  burstTime array.
 	 */
-	for(int i =0; i<threadsCreated; i++)
+	for(int i=0; i<threadsCreated; i++)
 	{
 		printf("\nBurst time for Thread %d: ", i);
 		scanf("%d", &burstTime[i]);
 	}
-
 
 	/*
 	 * Create Timer thread
@@ -128,7 +124,7 @@ int main(int argc, char **argv)
 	pthread_t timer_thread;
 	pthread_attr_t timer_attr;
 	pthread_attr_init(&timer_attr);
-	pthread_create(&timer_thread, &timer_attr, timerTime, NULL);
+	pthread_create(&timer_thread, &timer_attr, timerFunction, NULL);
 
 	/*
 	 * Loop and create requested threads.
@@ -137,10 +133,12 @@ int main(int argc, char **argv)
 	pthread_attr_t attr[threadsCreated];
 	for(int i=0; i<threadsCreated; i++)
 	{
-	  pthread_attr_init(&attr[i]);
-	  pthread_attr_setscope(&attr[i], PTHREAD_SCOPE_SYSTEM);
-	  pthread_attr_setschedpolicy(&attr[i], SCHED_RR);
-	  pthread_create(&threads[i], &attr[i], threadTime, &i);
+		int *arg = malloc(sizeof(*arg)); //Convert int to a pointer
+		*arg = i;
+		pthread_attr_init(&attr[i]);
+		pthread_attr_setscope(&attr[i], PTHREAD_SCOPE_SYSTEM);
+		pthread_attr_setschedpolicy(&attr[i], SCHED_RR);
+		pthread_create(&threads[i], &attr[i], threadRunner, arg);
 	}
 	pthread_join(timer_thread, NULL);
 }
